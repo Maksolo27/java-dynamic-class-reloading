a4086df225f22b80d4bf43e099ed70ea0a76d9c2
Maksym <maksym.koval@syncwords.com>
2022-10-04 14:16:42 +0000

    refactor

--- src/main/java/com/javaclassloadingexample/App.java
+++ src/main/java/com/javaclassloadingexample/App.java
@@ -3,15 +3,15 @@ package com.javaclassloadingexample;
 public class App {
     public static void main( String[] args ) {
     	for (;;) {
-    		    Class<?> myStaticClass = new Reloader().loadClass("com.javaclassloadingexample.MyStaticClass");
-    		    try {
-	    		    Object obj = myStaticClass.newInstance();
-					System.out.println (obj.toString ());
-	    		    Thread.sleep(2000);
+    		Class<?> myStaticClass = new Reloader().loadClass("com.javaclassloadingexample.MyStaticClass");
+    		try {
+    			Object obj = myStaticClass.newInstance();
+    			System.out.println (obj.toString ());
+    			Thread.sleep(2000);
 
-				} catch (Exception e1) {
-					e1.printStackTrace();
-				}
+    		} catch (Exception e1) {
+    			e1.printStackTrace();
+    		}
 		  }    
 	  }
 }
